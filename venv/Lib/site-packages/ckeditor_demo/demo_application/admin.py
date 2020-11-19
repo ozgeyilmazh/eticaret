b3add6f3aedb170f7b837d929c92c2ac7eb20664
onderyilmz <onder.yilmz@gmail.com>
2020-11-19 08:05:53 +0300

    Iyzico ödeme sistemi entegrasyonu yapıldı

--- venv/Lib/site-packages/ckeditor_demo/demo_application/admin.py
+++
@@ -1,8 +0,0 @@
-from __future__ import absolute_import
-
-from django.contrib import admin
-
-from . import models
-
-admin.site.register(models.ExampleModel)
-admin.site.register(models.ExampleNonUploadModel)
